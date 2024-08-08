c42681cb20f80321cd49a77c0bd68792d0653974
T-Godwins <gtuyishime@gmail.com>
2024-08-08 18:24:30 -0400

    improved landing page

--- app/page.js
+++ app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
       <Typography variant="h2" gutterBottom p={2} sx={{ textAlign:"center", fontSize: {xs:'2rem', md:'3rem', lg:'5rem'}}} >
         Welcome to your personal Dietitian
       </Typography>
-      <Button onClick={handleOpen} sx={{ fontSize: {xs:'20px', md:'25px', lg:'30px'}, borderRadius: '15px' }}>Start Chatting</Button>
+      <Button onClick={handleOpen} sx={{ fontSize: {xs:'20px', md:'25px', lg:'30px'}, borderRadius: '15px', color:"crimson", boxShadow:"0px 0px 10px rgba(0, 0, 0, 0.1)" }}>Start Chatting</Button>
       <Chatbot open={open} handleClose={handleClose} />
     </Box>
   );
