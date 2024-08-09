77f0337b18fc2ec8e7da3b4a9c95b2eecf244ed2
T-Godwins <gtuyishime@gmail.com>
2024-08-08 22:19:19 -0400

    Fix Landing

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
     >
       {/* <Login/> */}
       <Typography variant="h2" gutterBottom p={2} sx={{ textAlign:"center", fontSize: {xs:'2rem', md:'3rem', lg:'5rem'}}} >
-        Welcome to your personal Career Coach
+        Welcome to your own Career Coach
       </Typography>
       <Button onClick={handleOpen} sx={{ fontSize: {xs:'20px', md:'25px', lg:'30px'}, borderRadius: '15px', color:"crimson", boxShadow:"0px 0px 10px rgba(0, 0, 0, 0.1)" }}>Start Chatting</Button>
       <Chatbot open={open} handleClose={handleClose} />
