d562423b949484bb0425ed3f7a25c8015fed1bc1
T-Godwins <gtuyishime@gmail.com>
2024-08-10 16:45:52 -0400

    11 labs API backend works

--- app/api/11labs/route.js
+++ app/api/11labs/route.js
@@ -6,9 +6,23 @@
 
 // const audioStream = await elevenlabs.generate({
 //     stream: true,
-//     voice: "Bella",
+//     voice: "Adam",
 //     text: "This is a... streaming voice",
 //     model_id: "eleven_multilingual_v2"
 //   });
   
 //   stream(audioStream)
+
+import { ElevenLabsClient, ElevenLabs } from "elevenlabs";
+
+const client = new ElevenLabsClient({ apiKey: process.env.ELEVENLABS_API_KEY });
+await client.textToSpeech.convert("pMsXgVXv3BLzUgSXRplE", {
+    optimize_streaming_latency: ElevenLabs.OptimizeStreamingLatency.Zero,
+    output_format: ElevenLabs.OutputFormat.Mp32205032,
+    text: "It sure does, Jackie\u2026 My mama always said: \u201CIn Carolina, the air's so thick you can wear it!\u201D",
+    voice_settings: {
+        stability: 0.1,
+        similarity_boost: 0.3,
+        style: 0.2
+    }
+});
